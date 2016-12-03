b4a00c967a692ee9d673c456a477b2d66c812829
Peter Durrant <peterjdurrant@gmail.com>
2016-12-03 16:40:12 +0000

    Minor cleanup

--- MefRibbon/RibbonItems/ApplicationMenu/Help.xaml.cs
+++ MefRibbon/RibbonItems/ApplicationMenu/Help.xaml.cs
@@ -1,23 +1,22 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Ribbon;
 using MefRibbon.Common;
 
 namespace MefRibbon.RibbonItems.ApplicationMenu
 {
-    /// <summary>
-    /// Interaction logic for HelpApplicationMenuItem.xaml
-    /// </summary>
-    public partial class HelpApplicationMenuItem : RibbonApplicationMenuItem, IRibbonMenuItem
-    {
-        public HelpApplicationMenuItem()
-        {
-            InitializeComponent();
-        }
+   /// <summary>
+   ///    Interaction logic for HelpApplicationMenuItem.xaml
+   /// </summary>
+   public partial class HelpApplicationMenuItem : RibbonApplicationMenuItem, IRibbonMenuItem
+   {
+      public HelpApplicationMenuItem()
+      {
+         InitializeComponent();
+      }
 
-        private void Help_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("You've been helped!");
-        }
-    }
+      private void Help_Click(object sender, RoutedEventArgs e)
+      {
+         MessageBox.Show("You've been helped!");
+      }
+   }
 }
--- MefRibbon/RibbonItems/Buttons/Paste.xaml.cs
+++ MefRibbon/RibbonItems/Buttons/Paste.xaml.cs
@@ -1,22 +1,20 @@
-﻿using System.ComponentModel.Composition;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Controls.Ribbon;
 using MefRibbon.Common;
 
 namespace MefRibbon.RibbonItems.Buttons
 {
-    [RibbonLocation("Main", "Text Manipulation")]
-    public partial class Paste : RibbonButton, IRibbonItem
-    {
-        public Paste()
-        {
-            InitializeComponent();
-        }
+   [RibbonLocation("Main", "Text Manipulation")]
+   public partial class Paste : RibbonButton, IRibbonItem
+   {
+      public Paste()
+      {
+         InitializeComponent();
+      }
 
-        private void Paste_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Nothing was actually pasted!");
-        }
-    }
+      private void Paste_Click(object sender, RoutedEventArgs e)
+      {
+         MessageBox.Show("Nothing was actually pasted!");
+      }
+   }
 }
